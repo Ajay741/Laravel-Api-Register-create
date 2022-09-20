be4404b382cee3d1b9ad9601e11d0656cc1d97d1
Ajay <ajaysanjaysingh12345@gmail.com>
2022-09-20 14:20:39 +0530

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|string|min:2|max:100',
+            'email'=>'required|string|email|max:100|unique:users',
+            'password'=>'required|string|min:6|confirmed'
+
+        ]);
+        if($validator->fails())
+        {
+            return response()->json($validator->errors(),400);
+        }
+
+        $user = User::create([
+            'name' =>$request->name,
+            'email' =>$request->email,
+            'password'=>Hash::make($request->password)
+
+        ]);
+
+        return response()->json([
+            'message'=> 'User registered Successfully',
+            'user'=>$user
+        ]);
+        
+    }
+}
